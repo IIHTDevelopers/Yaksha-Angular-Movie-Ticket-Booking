571b96341bae85637a2568b42ab8a8128b7302a3
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2022-07-16 14:33:24 +0530

    Update footer.component.spec.ts

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -18,7 +18,7 @@ describe('FooterComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-  describe("bounday", () => {
+  describe("boundary", () => {
 
     it('should create', () => {
       expect(component).toBeTruthy();
